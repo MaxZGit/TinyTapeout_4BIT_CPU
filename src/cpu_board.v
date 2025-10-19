fb62af9e5079606157233a9c2f1e270f1aa94270
Max_Laptop <maxzaglmayr@gmail.com>
2025-10-19 23:41:56 +0200

    added new top level

---
+++ src/cpu_board.v
@@ -0,0 +1,37 @@
+
+module tt_um_four_bit_cpu_top_level(
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+    localparam OPERATION_CODE_WIDTH = 3;
+    localparam CRA_BIT_NUMB = 4;
+    localparam REGISTER_WIDTH = 4;
+    localparam MEMORY_ADDRESS_WIDTH = 4;
+    localparam MEMORY_REGISTERS = 16;
+
+  cpu #(
+        .CRA_BIT_NUMB(CRA_BIT_NUMB),
+        .OPERATION_CODE_WIDTH(OPERATION_CODE_WIDTH),
+        .REGISTER_WIDTH(REGISTER_WIDTH),
+        .MEMORY_ADDRESS_WIDTH(MEMORY_ADDRESS_WIDTH),
+        .MEMORY_REGISTERS(MEMORY_REGISTERS)
+    ) dut (
+        .clk_i(clk),
+        .reset_i(rst_n),
+
+        .in_pins_i(ui_in[3:0]),
+        .out_pins_o(uo_out[3:0]),
+
+        .bl_programm_i(ui_in[4]),
+        .bl_data_i(4'b0000),
+        .bl_address_i(4'b0000),
+        .bl_write_en_mem_i(1'b0)
+    );
+
+endmodule
---
+++ src/uart_rx.v
@@ -0,0 +1,150 @@
+module uart_rx #(
+    parameter UART_BAUD_RATE = 19200,
+    parameter UART_DATA_LENGTH = 8,
+    parameter CLK_FREQ = 10000000, //10 MHz
+    parameter RX_COUNTER_BITWIDTH = 3,
+    parameter BAUD_COUNTS_PER_BIT = 521,
+    parameter BAUD_RATE_COUNTER_BITWIDTH = 10
+) (
+    input wire clk_i,
+    input wire reset_i,
+
+    // IN
+    input wire rx_i,
+
+    // OUT
+    output wire [UART_DATA_LENGTH-1:0] data_o,
+    output wire data_valid_strb
+);
+
+    // ###########################################################
+    //                SIGNAL DEFINITION
+    // ###########################################################
+
+    // Constant definition
+    localparam integer BAUD_COUNTS_PER_BIT_HALF = BAUD_COUNTS_PER_BIT / 2;
+
+    // State encoding
+    localparam stIDLE       = 2'b00;
+    localparam stSTARTBIT   = 2'b01;
+    localparam stRECEIVING  = 2'b10;
+    localparam stSTOPBIT    = 2'b11;
+
+    // Internal Signals
+    // register for fsm
+    reg[1:0] rx_state;
+    reg[1:0] next_rx_state;
+
+    // register to store the number of incoming bits
+    reg [RX_COUNTER_BITWIDTH-1:0] rx_counter_val;
+    reg [RX_COUNTER_BITWIDTH-1:0] next_rx_counter_val;
+
+    // counting baudrate
+    reg [BAUD_RATE_COUNTER_BITWIDTH-1:0] baud_counter_val;
+    reg [BAUD_RATE_COUNTER_BITWIDTH-1:0] next_baud_counter_val;
+
+    // data
+    reg [UART_DATA_LENGTH-1:0] rx_data;
+    reg [UART_DATA_LENGTH-1:0] next_rx_data;
+    
+    // ###########################################################
+    //                COMBINATORIAL LOGIC (REGISTER)
+    // ###########################################################
+
+    // next rx_state logic
+    always @(rx_state, baud_counter_val, rx_counter_val, rx_i) begin
+        //default assignment
+        next_rx_state = rx_state;
+
+        case (rx_state)
+            stIDLE: begin
+                // wait for start bit
+                if (rx_i = 0)
+                    next_rx_state = stSTARTBIT;
+            end
+
+            stSTARTBIT: begin
+                // wait for baud counter to count 1 bit
+                if (baud_counter_val = BAUD_COUNTS_PER_BIT)
+                    next_rx_state = stRECEIVING;
+            end
+
+            stRECEIVING: begin
+                // store data at half baud counter val
+                if (rx_counter_val = UART_DATA_LENGTH - 1)
+                    next_rx_state = stSTOPBIT;
+            end
+
+            stSTOPBIT: begin
+                // wait for 1 bit
+                if (baud_counter_val = BAUD_COUNTS_PER_BIT)
+                    next_rx_state = stIDLE;
+            end
+        endcase
+    end
+
+    // baud counter value logic
+    always @(baud_counter_val, rx_state) begin
+        // default assignment
+        next_baud_counter_val = baud_counter_val;
+        
+        if (rx_state == stIDLE || baud_counter_val >= BAUD_COUNTS_PER_BIT)
+            next_baud_counter_val = {BAUD_RATE_COUNTER_BITWIDTH{1'b0}};
+        else  // if counter is smaller (and not in idle state), add one
+            next_baud_counter_val = baud_counter_val + 1;
+        else 
+    end
+
+    // rx_counter_val logic
+    always @(rx_counter_val, rx_state, baud_counter_val) begin
+        // default assignment
+        next_rx_counter_val = rx_counter_val;
+        
+        if (rx_state == stRECEIVING) begin
+            if (baud_counter_val = BAUD_COUNTS_PER_BIT) // count up after a full bit
+                next_rx_counter_val = rx_counter_val + 1;
+        end else begin // reset if not in receiving state
+            next_baud_counter_val = {BAUD_RATE_COUNTER_BITWIDTH{1'b0}};
+        end 
+    end
+
+    // receiving data logic
+    always @(rx_state, baud_counter_val, rx_i, rx_data) begin
+        // default assignment
+        next_rx_data = rx_data;
+
+        if (rx_state == stRECEIVING) begin
+            if (baud_counter_val = BAUD_COUNTS_PER_BIT_HALF)
+                next_rx_data = {rx_i, rx_data[UART_DATA_LENGTH - 1:1]};
+        end
+    end
+
+    // TODO MAYBE MAKE STROBE EVERY 4 BIT TO STORE IN MEMORY
+    // data valid strobe logic
+    always @(rx_state, next_rx_state) begin
+        if (rx_state == stSTOPBIT && next_rx_state == stIDLE)
+            data_valid_strb = 1;
+        else
+            data_valid_strb = 0;
+    end
+
+    assign data_o = rx_data;
+
+    // ###########################################################
+    //                SEQUENTIAL LOGIC (REGISTER)
+    // ###########################################################
+
+    always @(posedge clk_i or posedge reset_i) begin
+        if (reset_i) begin
+            rx_counter_val <= {RX_COUNTER_BITWIDTH{1'b0}};
+            baud_counter_val <= {BAUD_RATE_COUNTER_BITWIDTH{1'b0}};
+            rx_state <= stIDLE
+            rx_data <= {UART_DATA_LENGTH{1'b0}};
+        end else begin
+            rx_counter_val <= next_rx_counter_val;
+            baud_counter_val <= next_baud_counter_val;
+            rx_state <= next_rx_state;
+            rx_data <= next_rx_data;
+        end
+    end
+endmodule
